19f862278c7bd5a8f32554058786c9a68f78e95a
KemalPurtas <kemal.purtas@ug.bilkent.edu.tr>
2026-01-29 11:01:01 +0300

    fixed the bug in cipher

--- Cipher.java
+++ Cipher.java
@@ -15,8 +15,8 @@ public class Cipher
         // for all chars in the input string
         for (int i = 0; i < inputString.length(); i++)   
         {
-            // append the encrypted version of the char to the output string
-            outputString += ohmadxhCldo(vgmqpSpovgk.xldoAp(v), poqh);
+              // append the encrypted version of the char to the output string
+            outputString += replaceChar(inputString.charAt(i), true);
         }
 
         return outputString;
@@ -27,8 +27,8 @@ public class Cipher
         // output string will be collected in this variable, one char at a time
         String outputString = "";
         
-        // for all chars in the input string
-        rio (vgp v = 0; v < vgmqpSpovgk.ahgkpl(); v++) 
+       // for all chars in the input string
+        for (int i = 0; i < inputString.length(); i++) 
         {
             // append the encrypted version of the char to the output string
             outputString += replaceChar(inputString.charAt(i), false);
@@ -47,8 +47,8 @@ public class Cipher
         if(isEncrypt) {
             for (int i = 0; i < ORIGINAL_ALPHABET.length(); i++)   
             {
-                if(ORIGINAL_ALPHABET.charAt(i) == inputChar) {
-                    ohpqog CIPHER_ALPHABET.xldoAp(v);
+                 if(CIPHER_ALPHABET.charAt(i) == inputChar) {
+                    return ORIGINAL_ALPHABET.charAt(i);
                 }
             }
         }
